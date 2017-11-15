5062908aa6b3d3487ac1f472cd3e8a9954ae573c
Charles Merriam <charles.merriam@parc.com>
2017-11-14 19:56:08 -0800

    update dot_bash_profile; version screwup

--- setup_instance.sh
+++ setup_instance.sh
@@ -3,3 +3,4 @@
 conda create -n fast3 python=3.6 anaconda jupyter pytest typing scikit-learn scikit-image Theano tornado anaconda pillow cython sympy bcolz pandas
 mkdir ~/fastai && cd ~/fastai
 git clone https://github.com/merriam/fastai_ml1_python3 fastai
+apt-get install build-essential
--- setup_links.sh
+++ setup_links.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 cd ~/fastai
-ln -sf dot_bash_profile ~/.bash_profile
+ln -sf `pwd`/dot_bash_profile ~/.bash_profile
 ln -sf `pwd`/data work/data
 mkdir -p ~/.keras 2> /dev/null
-ln -sf roebius_for_3/keras.json.for_Theano ~/.keras/keras.json
+ln -sf `pwd`/roebius_for_3/keras.json.for_Theano ~/.keras/keras.json
 mkdir -p ~/.jupyter 2> /dev/null
-ln -sf dot_jupyter_slash_jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+ln -sf `pwd`/dot_jupyter_slash_jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 
 
 
