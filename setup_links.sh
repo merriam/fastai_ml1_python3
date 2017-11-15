9473b7ab73443ba84186d37e90c8606dc33edade
Charles Merriam <charles.merriam@parc.com>
2017-11-14 19:29:45 -0800

    more

--- setup_links.sh
+++ setup_links.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd ~/fastai
+ln -sf dot_bash_profile ~/.bash_profile
 ln -sf `pwd`/data work/data
 mkdir -p ~/.keras 2> /dev/null
 ln -sf roebius_for_3/keras.json.for_Theano ~/.keras/keras.json
